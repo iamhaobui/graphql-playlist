9b8214a52ac8df078cfed20e3c8ff4fefd23ddfe
haobui <iamhaobui@gmail.com>
2019-12-08 23:31:04 -0500

    updated

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const schema = require('./server/schema/schema');
 
 const app = express();
+
+app.use(cors());
 
 app.use(
   '/graphql',
