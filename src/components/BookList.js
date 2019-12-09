9398f86cfa36f662b0608671fed60f40e7be17b2
haobui <iamhaobui@gmail.com>
2019-12-08 23:29:28 -0500

    show a book list

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,24 +1,42 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import { getBooksQuery } from '../queries';
+import BookDetails from './BookDetails';
 
 class BookList extends Component {
-  displayBooks() {
-    const data = this.props.data;
+  constructor() {
+    super();
+    this.state = {
+      bookId: null,
+    };
+  }
+
+  onSelect = e => {
+    e.preventDefault();
+    this.setState({
+      bookId: e.target.id,
+    });
+  };
+
+  displayBooks = () => {
+    const { data } = this.props;
     if (data.loading) {
-      return <div>Loading data ...</div>;
+      return <div>Loading book ...</div>;
     }
     return data.books.map(book => {
-      return <li key={book.id}>{book.name}</li>;
+      return (
+        <li key={book.id} id={book.id} onClick={this.onSelect}>
+          {book.name}
+        </li>
+      );
     });
-  }
+  };
 
   render() {
     return (
       <>
-        <ul id="book-list">
-          <li>{this.displayBooks()}</li>
-        </ul>
+        <ul id="book-list">{this.displayBooks()}</ul>
+        <BookDetails bookId={this.state.bookId} />
       </>
     );
   }
