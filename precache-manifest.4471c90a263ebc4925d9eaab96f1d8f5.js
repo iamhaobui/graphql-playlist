d9a0e26c2e317acb2ba0c1b3790ed226285ed809
haobui <iamhaobui@gmail.com>
2019-12-08 23:41:07 -0500

    Updates

---
+++ precache-manifest.4471c90a263ebc4925d9eaab96f1d8f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b6280ec20c900d09b23266eb17b215e",
+    "url": "/graphql-playlist/index.html"
+  },
+  {
+    "revision": "7b7021fd31f0996a4648",
+    "url": "/graphql-playlist/static/css/main.d2040fb7.chunk.css"
+  },
+  {
+    "revision": "2da6c8343259fda34d43",
+    "url": "/graphql-playlist/static/js/2.0cb0adc7.chunk.js"
+  },
+  {
+    "revision": "cc73cd5b1041d17c21196a910826528d",
+    "url": "/graphql-playlist/static/js/2.0cb0adc7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7b7021fd31f0996a4648",
+    "url": "/graphql-playlist/static/js/main.f86b45b4.chunk.js"
+  },
+  {
+    "revision": "046419172894335f33c6",
+    "url": "/graphql-playlist/static/js/runtime-main.8a105288.js"
+  }
+]);
